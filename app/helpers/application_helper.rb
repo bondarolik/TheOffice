5dd03990b7837acba1ec5031f69fc186c65e74d2
V.Bruk <vbruk@hey.com>
2023-04-12 14:35:57 -0300

    Add TWUI markup for logged user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def navigation_items
+    [
+      { body: 'Menu Item', href: root_path },
+    ]
+  end
+
+  def navigation_class(path)
+    if current_page?(path)
+      "put your class here"
+    else
+      "non active class here"
+    end
+  end
+
+  def navigation_area(path)
+    current_page?(path) ? "area-current='page'" : "aria-current='false'"
+  end
 end
---
+++ app/javascript/controllers/canvas_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave } from "el-transition";
+
+export default class extends Controller {
+  static targets = ["container", "backdrop", "panel", "closeButton"];
+  connect() {
+    console.log("Canvas controller connected");
+  }
+
+  show() {
+    this.containerTarget.classList.remove("hidden");
+    enter(this.backdropTarget);
+    enter(this.closeButtonTarget);
+    enter(this.panelTarget);
+  }
+
+  hide() {
+    Promise.all([
+      leave(this.backdropTarget),
+      leave(this.closeButtonTarget),
+      leave(this.panelTarget),
+    ]).then(() => {
+      this.containerTarget.classList.add("hidden");
+    });
+  }
+}
